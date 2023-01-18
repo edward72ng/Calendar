1d2fcb067fa9dce8f61fa4e8dbe2812a95edff48
edward72ng <110264608+edward72ng@users.noreply.github.com>
2023-01-17 22:44:09 -0400

    SectionHome functions added

--- src/app/SectionHome.js
+++ src/app/SectionHome.js
@@ -2,13 +2,13 @@ import React from "react";
 import {OneTodo} from './OneTodo'
 function SectionHome({dataVAlues, functions}) {
     const {tittle, data} = dataVAlues
-    
+    const {updateBlocs} = functions
     return <>
         <div>{tittle}</div>
         {
             data.map((elem, i)=>{
                 return (
-                    <OneTodo id={elem.id} content={elem.content} details ={elem.deatils} evento={elem.evento}>
+                    <OneTodo id={elem.id} content={elem.content} details ={elem.deatils} evento={elem.evento} updateBlocs={updateBlocs}>
 
                     </OneTodo>
                 )
