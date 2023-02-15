45878aac8a8a2d4003f57c298dfc92d5af7ec6e9
= <local@edward.com>
2023-02-15 02:35:03 +0100

    vpHeight added

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,14 @@ import Aplication from './Aplication';
 import { ItemsProvider } from '../providers/ItemsContext';
 
 function App () {
+
+    const vpHeight = window.innerHeight;
+        const style = document.createElement("style");
+        style.innerHTML = `:root {
+          --vp-height: ${vpHeight}px;
+        }`;
+        document.head.appendChild(style);
+
     return(
             <Routes>
                 <Route path='/' element={<Login/>}/>
